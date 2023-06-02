b79b559ac9a67efd866ee0fed4078986d72a30f9
latz09 <85132328+latz09@users.noreply.github.com>
2023-06-02 09:21:14 -0500

    about text

--- components/About.js
+++ components/About.js
@@ -2,16 +2,16 @@ const About = () => {
 	return (
 		<div className='max-w-5xl mx-auto px-4  text-lg lg:text-2xl text-dark tracking-wide'>
 			<div className='grid gap-8'>
-				<div className='text-secondary   font-bold tracking-widest font-questrial text-2xl scale-y-150  uppercase italic text-center'>
+				<div className='text-secondary   font-bold tracking-widest font-oswald text-2xl scale-y-150  uppercase italic text-center'>
 					Your Local Handyman
 				</div>
-				<div className='leading-10 opacity-70 gap-2 grid'>
+				<div className='leading-10 text-dark/70 font-bold font-oswald grid gap-2'>
 					<div>
 						<span className='font-bold text-primary'>
 							For more than 20 years{' '}
 						</span>
 						I have honed my skills in the construction industry alongside
-						leading contractors. However, since 2012, my focus has been to{' '}
+						leading contractors. Since 2012, my focus has been to{' '}
 						<span className='font-bold text-primary'>
 							{' '}
 							work directly for you!{' '}
